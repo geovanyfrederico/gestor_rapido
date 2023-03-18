3132f8a2949d21fe6146caab3993f4c743aaa19a
Xavier36 <122737161+geovanyfrederico@users.noreply.github.com>
2023-03-18 14:15:55 +0100

    Por concluir

--- app/lib/src/views/auth/registar.dart
+++ app/lib/src/views/auth/registar.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:gr/src/config/app_theme.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:gr/src/controllers/AuthController.dart';
+import 'package:gr/src/views/auth/entrar.dart';
+import 'package:gr/theme/theme1.dart';
+
+import 'senha.dart';
 
 class AuthRegistarView extends StatelessWidget {
   final AuthController controller =
@@ -10,9 +14,188 @@ class AuthRegistarView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Registar"),
-      ),
+      body: Container(
+          color: Theme1.cardTitleBg,
+          padding: EdgeInsets.only(top: 60, left: 40, right: 40),
+          child: Form(
+            key: controller.formLogin,
+            child: ListView(
+              children: [
+                SizedBox(
+                    width: 128,
+                    height: 128,
+                    child: Image.asset('assets/images/gestor.png')),
+                Text(
+                  'Criar uma nova conta',
+                  style: Theme1.h1TextStyle(),
+                  textAlign: TextAlign.center,
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 70,
+                  child: TextFormField(
+                    obscureText: false,
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                      focusColor: Theme1.primary,
+                      prefixIcon: Icon(Icons.person),
+                      fillColor: Theme1.gray,
+                      filled: true,
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Theme1.primary, width: 2.0),
+                      ),
+                      border: InputBorder.none,
+                      labelText: 'Nome de usuário',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Campo obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 70,
+                  child: TextFormField(
+                    obscureText: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      focusColor: Theme1.primary,
+                      prefixIcon: Icon(Icons.email),
+                      fillColor: Theme1.gray,
+                      filled: true,
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Theme1.primary, width: 2.0),
+                      ),
+                      border: InputBorder.none,
+                      labelText: 'Email',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Campo obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 70,
+                  child: TextFormField(
+                    obscureText: true,
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    maxLength: 4,
+                    decoration: const InputDecoration(
+                      focusColor: Theme1.primary,
+                      prefixIcon: Icon(Icons.lock),
+                      fillColor: Theme1.gray,
+                      filled: true,
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Theme1.primary, width: 2.0),
+                      ),
+                      border: InputBorder.none,
+                      labelText: 'Pin',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Campo obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  width: 200,
+                  height: 70,
+                  child: TextFormField(
+                    obscureText: true,
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    maxLength: 4,
+                    decoration: const InputDecoration(
+                      focusColor: Theme1.primary,
+                      prefixIcon: Icon(Icons.lock),
+                      fillColor: Theme1.gray,
+                      filled: true,
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Theme1.primary, width: 2.0),
+                      ),
+                      border: InputBorder.none,
+                      labelText: 'Confirmar Pin',
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Campo obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 60,
+                  alignment: Alignment.center,
+                  child: SizedBox.expand(
+                    child: GFButton(
+                      onPressed: () {
+                        controller.login();
+                        //Get.offNamed('/details');
+                      },
+                      text: "Cadastre-se",
+                      color: Theme1.primary,
+                      size: GFSize.SMALL,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 60,
+                  alignment: Alignment.center,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AuthEntrarView()));
+                    },
+                    child: Text(
+                      "Já tem uma conta?",
+                      style: TextStyle(color: Theme1.primary),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
--- app/lib/src/views/auth/senha.dart
+++ app/lib/src/views/auth/senha.dart
@@ -1,18 +1,121 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:gr/src/config/app_theme.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:gr/src/controllers/AuthController.dart';
+import 'package:gr/src/views/auth/entrar.dart';
+import 'package:gr/theme/theme1.dart';
 
 class AuthSenhaView extends StatelessWidget {
   final AuthController controller =
       Get.put(AuthController()); // Adicione o controller aqui
   AuthSenhaView({super.key});
+  int _value = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Recuperar Senha"),
-      ),
-    );
+        body: Container(
+            color: Theme1.cardTitleBg,
+            padding: EdgeInsets.only(top: 60, left: 40, right: 40),
+            child: Form(
+                key: controller.formLogin,
+                child: ListView(children: [
+                  SizedBox(
+                      width: 128,
+                      height: 128,
+                      child: Image.asset('assets/images/gestor.png')),
+                  Text(
+                    'Repor o teu Pin',
+                    style: Theme1.h1TextStyle(),
+                    textAlign: TextAlign.center,
+                  ),
+                  const Divider(),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Como pretende recuperar o teu Pin?',
+                    style: Theme1.h3TextStyle(),
+                    textAlign: TextAlign.center,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RadioListTile(
+                        value: 1,
+                        groupValue: _value,
+                        onChanged: (value) {
+                          setState(() {
+                            _value == value;
+                          });
+                        },
+                        activeColor: Theme1.primary,
+                        title: Text("Radio 1"),
+                        subtitle: Text("Sub title Radio 1"),
+                      ),
+                      RadioListTile(
+                        value: 2,
+                        groupValue: _value,
+                        onChanged: (value) {
+                          setState(() {
+                            _value == value;
+                          });
+                        },
+                        activeColor: Theme1.primary,
+                        title: Text("Radio 2"),
+                        subtitle: Text("Sub title Radio 2"),
+                      ),
+                      RadioListTile(
+                        value: 3,
+                        groupValue: _value,
+                        onChanged: (value) {
+                          setState(() {
+                            _value == value;
+                          });
+                        },
+                        activeColor: Theme1.primary,
+                        title: Text("Radio 3"),
+                        subtitle: Text("Sub title Radio 3"),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 60,
+                    alignment: Alignment.center,
+                    child: SizedBox.expand(
+                      child: GFButton(
+                        onPressed: () {
+                          controller.login();
+                          //Get.offNamed('/details');
+                        },
+                        text: "Continuar",
+                        color: Theme1.primary,
+                        size: GFSize.LARGE,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 60,
+                    alignment: Alignment.center,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AuthEntrarView()));
+                      },
+                      child: Text(
+                        "Cancelar",
+                        style: TextStyle(color: Theme1.primary),
+                      ),
+                    ),
+                  ),
+                ]))));
   }
+
+  void setState(Null Function() param0) {}
 }
